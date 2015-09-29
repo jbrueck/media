c947ffd5121cca52ac70bf3e0503846d94bcce83
John van Breda <john.vanbreda@biodiverseit.co.uk>
2015-09-29 09:15:13 +0100

    Functionality for report_picker to display report metadata
    Intention is that the report picker will allow report metadata including
    the list of columns and parameters to be viewed for the selected report.

--- js/reportPicker.js
+++ js/reportPicker.js
@@ -12,22 +12,60 @@
  * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
  */
 
-/**
- * When clicking on an entry in the list of reports available, display the title and description of that report
- * in the metadata panel on the report_picker.
- */
-function displayReportMetadata(control, path) {
-  // safe for Windows paths
-  path = path.replace('\\','/');
-  path = path.split('/');
-  var current = indiciaData.reportList;
-  jQuery.each(path, function(idx, item) {
-    current = current[item];
-    if (current.type === 'report') {
-      jQuery('#' + control + ' .report-metadata').html('<strong>' + current.title+'</strong><br/>' +
+var displayReportMetadata;
+
+jQuery(document).ready(function($) {
+
+  /**
+   * When clicking on an entry in the list of reports available, display the title and description of that report
+   * in the metadata panel on the report_picker.
+   */
+  displayReportMetadata = function (control, path) {
+    // safe for Windows paths
+    path = path.replace('\\', '/');
+    path = path.split('/');
+    var current = indiciaData.reportList;
+    $.each(path, function (idx, item) {
+      current = current[item];
+      if (current.type === 'report') {
+        $('#' + control + ' .report-metadata').html('<strong>' + current.title + '</strong><br/>' +
           '<p>' + current.description + '</p>');
-    } else {
-      current = current['content'];
-    }
-  });
-}
+        $('#picker-more').show();
+      } else {
+        current = current['content'];
+      }
+    });
+  }
+
+  function showMoreInfo() {
+    var rpt = $('ul.treeview input:checked').val();
+    $.ajax({
+      dataType: "jsonp",
+      url: indiciaData.read.url+'index.php/services/report/requestReport',
+      data: {"reportSource":"local","report":rpt+'.xml', 'wantRecords':0, 'wantColumns':1, 'wantCount':0, 'wantParameters':1,
+        "auth_token":indiciaData.read.auth_token, "nonce":indiciaData.read.nonce},
+      success: function(data) {
+        if (typeof data.columns!=="undefined") {
+          var rows='';
+          $.each(data.columns, function(field, def) {
+            if (typeof def.display==="undefined") {
+              def.display='Not set';
+            }
+            rows += '<tr><th schope="row">' + field + '</th><td>' + def.display + '</td></tr>';
+          });
+          $.fancybox('<table class="report-metadata ui-widget"><caption>Report columns</caption>' +
+              '<thead class="ui-widget-header"><tr><th>Field</th><th>Title</th></tr></thead>' +
+              '<tbody>' + rows + '</tbody></table>');
+        }
+      },
+      error: function() {
+        alert('Report information could not be obtained');
+      }
+    });
+  }
+
+  $('#picker-more').click(showMoreInfo);
+
+  $('#picker-more').hide();
+
+});
