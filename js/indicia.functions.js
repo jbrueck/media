c49d641c8531d5a3f33dc427359cbbbc2763c122
John <john.vanbreda@biodiverseit.co.uk>
2016-04-05 09:02:59 +0100

    Fixes grid action loading on pages with no URL params
    Function for retrieving URL params returned the current URL
    as an undefined entry if there is no ? in the address. Now fixed.

--- js/indicia.functions.js
+++ js/indicia.functions.js
@@ -200,12 +200,15 @@ if (typeof window.indiciaData==="undefined") {
    * @returns {Array}
    */
   indiciaFns.getUrlVars = function() {
-    var vars = {}, hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-      hash = hashes[i].split('=');
-      vars[hash[0]] = hash[1];
+    var vars = {}, hash, 
+        splitPos = window.location.href.indexOf('?'),
+        hashes = window.location.href.slice(splitPos + 1).split('&');
+    if (splitPos!==-1) {
+      for(var i = 0; i < hashes.length; i++)
+      {
+        hash = hashes[i].split('=');
+        vars[hash[0]] = hash[1];
+      }
     }
     return vars;
   }
